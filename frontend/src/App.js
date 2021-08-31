87491a2878f6b90fd0ac61df5ddf091ed11dc9da
Sergey Gvozdev <gvozdev@yandex.ru>
2021-08-31 12:33:28 +0300

    Added header component.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import "./App.css";
+import Header from "./components/Header/Header"
 import { connect, sendMsg } from "./api";
 
 class App extends Component {
@@ -17,6 +18,7 @@ class App extends Component {
   render() {
     return (
         <div className="App">
+          <Header />
           <button onClick={this.send}>Click</button>
         </div>
     )
