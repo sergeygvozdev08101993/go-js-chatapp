2c0f8b4fa30618fdc8a1debc398118a0b5f598b4
Sergey Gvozdev <gvozdev@yandex.ru>
2021-08-31 15:50:52 +0300

    Added chat history component.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,15 +2,28 @@ import React, { Component } from "react";
 import "./App.css";
 import Header from "./components/Header/Header"
 import { connect, sendMsg } from "./api";
+import ChatHistory from "./components/ChatHistory/ChatHistory";
 
 class App extends Component {
 
   constructor(props) {
     super(props);
-    connect();
+    this.state = {
+        chatHistory: []
+    }
   }
 
-  send() {
+  componentDidMount() {
+      connect((msg) => {
+          console.log("new message")
+          this.setState(prevState => ({
+              chatHistory: [...this.state.chatHistory, msg]
+          }))
+          console.log(this.state);
+      });
+  }
+
+    send() {
     console.log("hello");
     sendMsg("hello");
   }
@@ -19,6 +32,7 @@ class App extends Component {
     return (
         <div className="App">
           <Header />
+            <ChatHistory chatHistory={this.state.chatHistory}/>
           <button onClick={this.send}>Click</button>
         </div>
     )
